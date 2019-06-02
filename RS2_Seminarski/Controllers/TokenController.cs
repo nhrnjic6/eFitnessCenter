2860edf81aabbe406c2c33abb5ea2e17f62c48e2
Nihad Hrnjic <nihad.hrnjic@klika.ba>
2019-06-02 11:44:27 +0200

    Add JWT token util for creating and validating tokens

--- RS2_Seminarski/Controllers/TokenController.cs
+++ RS2_Seminarski/Controllers/TokenController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RS2_Seminarski.Database;
+using RS2_Seminarski.Security;
 
 namespace RS2_Seminarski.Controllers
 {
@@ -22,31 +24,30 @@ namespace RS2_Seminarski.Controllers
 
         // GET: api/Token
         [HttpGet]
-        public IEnumerable<AppUser> GetAppUsers()
+        public String GetAppUsers()
         {
-            return _context.AppUsers
+            IEnumerable<AppUser> users = _context.AppUsers
                 .Include(user => user.Employee)
                 .Include(user => user.Client)
                 .ToList();
+
+            return JWTUtil.CreateToken(1, "EMPLOYEE");
         }
 
         // GET: api/Token/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetAppUser([FromRoute] int id)
+        public UserInfo GetAppUser([FromRoute] int id)
         {
-            if (!ModelState.IsValid)
+            var token = Request.Headers["Authorization"];
+            var claims = JWTUtil.VerifyToken(token).Claims;
+
+            UserInfo userInfo = new UserInfo
             {
-                return BadRequest(ModelState);
-            }
+                Id = int.Parse(claims.Where(x => x.Type == ClaimTypes.Name).FirstOrDefault().Value),
+                Role = claims.Where(x => x.Type == ClaimTypes.Role).FirstOrDefault().Value
+            };
 
-            var appUser = await _context.AppUsers.FindAsync(id);
-
-            if (appUser == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(appUser);
+            return userInfo;
         }
 
         // PUT: api/Token/5
---
+++ RS2_Seminarski/Security/JWTUtil.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS2_Seminarski.Security
+{
+    public static class JWTUtil
+    {
+        public static string CreateToken(int userId, string role)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var secretString = "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING";
+            byte[] buffer = Encoding.ASCII.GetBytes(secretString);
+
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor();
+            securityTokenDescriptor.Expires = DateTime.UtcNow.AddDays(1);
+            securityTokenDescriptor.SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(buffer), SecurityAlgorithms.HmacSha256Signature);
+            securityTokenDescriptor.Subject = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, userId.ToString()),
+                new Claim(ClaimTypes.Role, role)
+            });
+
+            SecurityToken securityToken = handler.CreateToken(securityTokenDescriptor);
+            return handler.WriteToken(securityToken);
+        }
+
+        public static ClaimsPrincipal VerifyToken(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var secretString = "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING";
+            byte[] buffer = Encoding.ASCII.GetBytes(secretString);
+
+            SecurityToken validatedToken;
+            var param = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(buffer),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true
+            };
+
+            try
+            {
+                return handler.ValidateToken(token, param, out validatedToken);
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
