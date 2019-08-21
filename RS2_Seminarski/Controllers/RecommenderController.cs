be95f740ec13e997ec62f021c2fa5b83f6f1874a
Nihad Hrnjic <nihad.hrnjic@klika.ba>
2019-08-21 10:45:47 +0200

    add person correlaction algorith and controller

---
+++ RS2_Seminarski/Controllers/RecommenderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RS2_Seminarski.Database;
+using RS2_Seminarski.Security;
+using RS2_Seminarski.Services;
+
+namespace RS2_Seminarski.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecommenderController : ControllerBase
+    {
+        private readonly FitnessCenterDbContext _context;
+        private readonly RecommenderService _recommenderService;
+        private readonly AuthenticationService _authenticationService;
+        private readonly IMapper _mapper;
+
+        public RecommenderController(FitnessCenterDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _recommenderService = new RecommenderService(_context, _mapper);
+            _authenticationService = new AuthenticationService(_context);
+        }
+
+        [HttpGet("suplements/{suplementId}")]
+        public List<Models.Suplements.Suplement> GetSimilarSuplements(int suplementId)
+        {
+            UserInfo userInfo = _authenticationService.IsAuthorized(Request, "CLIENT");
+            return _recommenderService.GetSimilarSuplements(suplementId, userInfo);
+        }
+    }
+}
--- RS2_Seminarski/Services/RecommenderService.cs
+++ RS2_Seminarski/Services/RecommenderService.cs
@@ -1,4 +1,7 @@
-﻿using RS2_Seminarski.Database;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using RS2_Seminarski.Database;
+using RS2_Seminarski.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +12,23 @@ namespace RS2_Seminarski.Services
     public class RecommenderService
     {
         private readonly FitnessCenterDbContext _context;
-        private Dictionary<int, List<SuplementsRating>> suplementRatings; 
+        private Dictionary<int, List<SuplementsRating>> suplementRatings;
+        private readonly IMapper _mapper;
 
-        public RecommenderService(FitnessCenterDbContext context)
+        public RecommenderService(FitnessCenterDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }  
 
-        public List<Models.Suplements.Suplement> GetSimilarSuplements(int suplementId)
+        public List<Models.Suplements.Suplement> GetSimilarSuplements(int suplementId, UserInfo userInfo)
         {
             List<Suplement> reccomendedSuplements = new List<Suplement>();
 
             // get ratings for all suplements other than current suplement
             List<Suplement> restOfSuplements = _context.Suplements
+                .Include(x => x.SuplementsRatings)
+                .Include(x => x.SuplementType)
                 .Where(x => x.SuplementTypeId == suplementId)
                 .ToList();
 
@@ -46,7 +53,7 @@ namespace RS2_Seminarski.Services
 
             foreach(var ratings in suplementRatings)
             {
-                List<SuplementsRating> rating1 = new List<SuplementsRating>();
+                List<SuplementsRating> rating1 = new List<SuplementsRating>(); 
                 List<SuplementsRating> rating2 = new List<SuplementsRating>();
 
                 foreach (var rating in currentSuplementRating)
@@ -66,11 +73,55 @@ namespace RS2_Seminarski.Services
                     reccomendedSuplements.Add(suplement);
                 }
             }
+
+            return reccomendedSuplements.Select(x => mapFromDb(x, userInfo)).ToList();
         }
 
         private double GetSimilarityBetweenProducts(List<SuplementsRating> rating1, List<SuplementsRating> rating2)
         {
-            throw new NotImplementedException();
+            if (rating1.Count != rating2.Count) return 0;
+
+            double numerator = 0, denominator1 = 0, denominator2 = 0;
+
+            double rating1Avg = rating1.Average(x => x.Rating);
+            double rating2Avg = rating2.Average(x => x.Rating);
+
+            for(int i = 0; i < rating1.Count; i++)
+            {
+                numerator += (rating1[i].Rating - rating1Avg) * (rating2[i].Rating - rating2Avg);
+                denominator1 += Math.Pow(rating1[i].Rating - rating1Avg, 2);
+                denominator2 += Math.Pow(rating2[i].Rating - rating2Avg, 2);
+            }
+
+            double denominator = Math.Sqrt(denominator1) * Math.Sqrt(denominator2);
+
+            if (denominator == 0) return 0;
+
+            return numerator / denominator;
+        }
+
+        private Models.Suplements.Suplement mapFromDb(Database.Suplement dbSuplement, UserInfo userInfo)
+        {
+            Models.Suplements.Suplement suplement = new Models.Suplements.Suplement();
+            _mapper.Map(dbSuplement, suplement);
+            suplement.SuplementTypeName = dbSuplement.SuplementType?.Type;
+            suplement.CreatedAt = dbSuplement.CreatedAt.ToString("dd-MM-yyyy");
+
+            if (dbSuplement.SuplementsRatings != null && dbSuplement.SuplementsRatings.Count > 0)
+            {
+                suplement.AverageRating = dbSuplement.SuplementsRatings.Average(x => x.Rating);
+
+                if (userInfo != null)
+                {
+                    SuplementsRating userSuplementRating = dbSuplement.SuplementsRatings.Where(x => x.ClientId == userInfo.Id).FirstOrDefault();
+                    if (userSuplementRating != null)
+                    {
+                        suplement.UserRating = userSuplementRating.Rating;
+                    }
+                }
+            }
+
+            return suplement;
         }
     }
 }
